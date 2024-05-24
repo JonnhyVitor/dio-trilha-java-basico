42baba2bd79efb44fd285eb00e0f80ea4943bcce
jonnhyvitor <jbs.tek.net@gmail.com>
2024-05-24 20:19:47 -0300

    Progeto terminalBanco java

---
+++ conta-banco/src/TerminalBanco.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TerminalBanco {
+
+    public static void main(String[] args) throws Exception {
+        //dados bancarios
+        int numeroDaConta=7070;
+        String agencias="666-6";
+        String nomedoCliente="joao";
+        double saldoBancario=1500;
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+         System.out.println("digite o numero da sua conta");
+         int numero =scanner.nextInt();
+
+         System.out.println("digite sua agencia");
+         String agencia=scanner.next();
+
+         System.out.println("digite seu nome");
+         String nomeCliente=scanner.next();
+
+    
+
+         System.out.println("ola,"+nomeCliente);
+         System.out.println("Obrigado por cria sua conta com a gente");
+         System.out.println("Aqui esta o numero da sua conta:"+numero);
+         System.out.println("sua agencia:" + agencia);
+         System.out.println("aqui esta seu saldo disponivel para saque:"+ saldoBancario);
+    
+
+    }
+}
