5620a43b06f025ff32c3eeb02633313966bccb02
jonnhyvitor <jbs.tek.net@gmail.com>
2024-05-24 20:44:40 -0300

    mais correções

--- conta-banco/src/TerminalBanco.java
+++ conta-banco/src/TerminalBanco.java
@@ -6,28 +6,28 @@ public class TerminalBanco {
     public static void main(String[] args) throws Exception {
         //dados bancarios
         int numeroDaConta=7070;
-        String agencias="666-6";
+        String agenciaCliente="666-6";
         String nomedoCliente="joao";
         double saldoBancario=1500;
         //adiconado Scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-         System.out.println("digite o numero da sua conta");
+         System.out.println("Por Favor,digite o numero da sua conta");
          int numero =scanner.nextInt();
 
-         System.out.println("digite sua agencia");
+         System.out.println("Por Favor,digite o numero da sua agencia");
          String agencia=scanner.next();
 
-         System.out.println("digite seu nome");
+         System.out.println("Por Favor,digite seu nome");
          String nomeCliente=scanner.next();
 
          //exibindo imformaçoes da conta 
 
-         System.out.println("ola,"+nomeCliente);
-         System.out.println("Obrigado por cria sua conta com a gente");
-         System.out.println("Aqui esta o numero da sua conta:"+numero);
-         System.out.println("sua agencia:" + agencia);
-         System.out.println("aqui esta seu saldo disponivel para saque:"+ saldoBancario);
+         System.out.println("Olá,"+nomeCliente);
+         System.out.println("Obrigado por cria sua conta em nosso banco");
+         System.out.println("Conta:"+numero);
+         System.out.println("Sua agencia é:" + agencia);
+         System.out.println("Aqui está seu saldo já disponivel para saque:"+ saldoBancario);
     
 
     }
